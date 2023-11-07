50c5199d50ca2e7ab972e28d6a95d863c98570cf
YogeshNaroal01 <yogeshnarola001@gmail.com>
2023-11-07 09:48:02 +0530

    c_lec_2

---
+++ PR_Q_1.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+ clrscr();
+ printf("yogesh narola\n");
+ printf("i ma 20 year's old\n");
+ printf("pramukhswami vidhyamandir sarangpur\n");
+ printf("fullstack developer\n");
+ printf("my grid : 4125\n");
+ printf("i am leave's in surat city\n");
+ getch();
+
+}
